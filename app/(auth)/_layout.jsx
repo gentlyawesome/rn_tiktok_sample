7faaa29664d712848276a9d7d408a9070bf9d260
Arc Angelo Ibalio <gentlyawesome@gmail.com>
2024-09-03 16:03:52 +0800

    initial commit

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function AuthLayout() {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen 
+        name="sign-in"
+        options={{
+          headerShown: false
+        }}
+        />
+        <Stack.Screen 
+        name="sign-up"
+        options={{
+          headerShown: false
+        }}
+        />
+      </Stack>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import {
+  Alert,
+  FlatList,
+  Image,
+  RefreshControl,
+  Text,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../../constants";
+import SearchInput from "../../components/SearchInput";
+import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
+
+export default function Home() {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
+
+  const [refreshing, setrefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setrefreshing(true);
+    await refetch();
+    setrefreshing(false);
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          // <Text className="text-3xl text-white">{item.title}</Text>
+          <VideoCard video={item} />
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between item-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-white text-2xl font-psemibold">
+                  {user?.username}
+                </Text>
+              </View>
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+            <SearchInput />
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                Latest Videos
+              </Text>
+              <Trending posts={latestPosts ?? []} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos created yet"
+          />
+        )}
+        refreshControl={<RefreshControl />}
+      />
+    </SafeAreaView>
+  );
+}
